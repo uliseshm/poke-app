639a1d7e6bb6bb8961cebcffcd97a3edc563545e
uliseshm <ulises.h.dev@gmail.com>
2025-08-01 10:43:59 -0600

    feat: se muestran los detalles del pokemon seleccionado

--- src/screens/PokemonDetailScreen.js
+++ src/screens/PokemonDetailScreen.js
@@ -1,13 +1,73 @@
 // src/screens/PokemonDetailScreen.js
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, ActivityIndicator } from 'react-native';
+import { getPokemonDetail } from '~/services/pokemonService';
 
 export default function PokemonDetailScreen({ route }) {
   const { pokemonName } = route.params;
+  const [pokemon, setPokemon] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    const fetchPokemonDetails = async () => {
+      try {
+        console.log("Fetching details for:", pokemonName);
+        const details = await getPokemonDetail(pokemonName);
+        setPokemon(details)
+        console.log("Respuesta de la API:", details);
+      } catch (error) {
+        console.error("Error al consultar los detalles del pokemon:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPokemonDetails();
+  }, [pokemonName]);
+
+  if (loading || !pokemon) {
+    return (
+      <View className='flex-1 justify-center items-center bg-gray-100'>
+        <ActivityIndicator size="large" color="#0000FF"/>
+      </View>
+    );
+  }
+
+  const imageUrl = pokemon?.sprites?.front_default;
 
   return (
-    <View className="flex-1 justify-center items-center bg-gray-100">
-      <Text className="text-xl">Detalles de {pokemonName}</Text>
+    <View className="flex-1 items-center p-4 bg-gray-100">
+      <View className='bg-white p-6 rounded-lg shadow-md items-center w-full max-w-sm'>
+        <Text className='text-3xl font-bold capitalize mb-4'>{pokemon.name}</Text>
+
+        <View className="w-40 h-40 relative">
+         
+          {imageUrl && (
+            <Image
+              className="w-full h-full"
+              source={{ uri: imageUrl }}
+              style={{ width: 160, height: 160 }} 
+            />
+          )}
+        </View>
+
+        <View className='mt-4 flex-row justify-center space-x-2'>
+          {pokemon.types.map((typeInfo, index) => (
+            <View key={index} className='bg-blue-500 rounded-full px-4 py-1'>
+              <Text className='text-white font-bold capitalize'>{typeInfo.type.name}</Text>
+            </View>
+          ))}
+        </View>
+        <View className='mt-6 w-full space-y-2'>
+          <Text className='text-lg'>
+            <Text className='font-bold'>Altura:</Text> {pokemon.heigth / 10} m
+          </Text>
+          <Text className='text-lg'>
+            <Text className='font-bold'>Peso:</Text> {pokemon.weight / 10} kg
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
--- src/services/pokemonService.js
+++ src/services/pokemonService.js
@@ -13,3 +13,17 @@ export const getPokemonList = async (limit = 20, offset = 0) => {
         throw error;
     }
 };
+
+export const getPokemonDetail = async (pokemonName) => {
+    try {
+        const response = await fetch(`${POKEAPI_BASE_URL}/pokemon/${pokemonName}`);
+        if (!response.ok) {
+            throw new Error(`No se pudo obtener los detalles del Pokémon ${pokemonName}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error al obtener los detalles del Pokémon ${pokemonName}:`, error);
+        throw error;
+    }
+};
